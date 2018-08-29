4e28c9da133f19d72384824afef1a1e1aa0ec05e
kevinm <kevin.mouton@just-eat.com>
2018-08-29 16:18:35 +1000

    add

---
+++ src/Templates/Api/Homedish.Template.Core/Services/TestService.cs
@@ -0,0 +1,17 @@
+﻿using Homedish.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace Homedish.Template.Core.Services
+{
+    public class TestService
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configurations;
+
+        public TestService(ILogger logger, IConfiguration configurations)
+        {
+            _logger = logger;
+            _configurations = configurations;
+        }
+    }
+}
---
+++ src/Templates/Api/Homedish.Template.UnitTests/Services/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Homedish.Template.UnitTests
+{
+    public class TestServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
